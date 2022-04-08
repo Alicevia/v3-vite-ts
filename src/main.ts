90d16477df0b1fcf87900690e70482c3ea865fcd
unknown <zhangcf@openx.com>
2022-04-08 18:12:36 +0800

    'add:vueuse'

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import store from './store'
 import router from './router'
 import App from './App.vue'
+import './utils/request'
 import './style/index.scss'
 const app = createApp(App)
 app.use(store)
---
+++ src/utils/request/index.ts
@@ -0,0 +1,4 @@
+import { useAxios } from '@vueuse/integrations'
+import axios from 'axios'
+
+const instance = axios.create({})
