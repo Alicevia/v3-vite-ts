6c9eee1900fcd5dfa075bdbee72ee34b32a5e5ff
zhangchengfei <ed-phoenix@qq.com>
2022-05-19 00:41:05 +0800

    'fix:eslint'

--- src/api/test.ts
+++ src/api/test.ts
@@ -1,14 +1,14 @@
 import { useAxios } from '@vueuse/integrations/useAxios'
 
 export const getTestJson = () => {
-  return useAxios('/public/test.json', {}, { immediate: false })
+  return useAxios('/public/test.json', {}, { immediate: false, })
 }
 
 export const getTest2Json = () => {
   return useAxios('/public/test2.json')
 }
 export const getTestError = () => {
-  return useAxios('/public/test42.json', {}, { immediate: false })
+  return useAxios('/public/test42.json', {}, { immediate: false, })
 }
 export const getUserInfo = () => {
   return useAxios('/public/auth.json')
